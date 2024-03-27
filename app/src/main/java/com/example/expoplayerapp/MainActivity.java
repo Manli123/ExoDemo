09929a2d457bea17b8149b55a30d0a49fa23e03c
Manli123 <32266311+Manli123@users.noreply.github.com>
2024-03-27 17:03:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/expoplayerapp/MainActivity.java
+++ app/src/main/java/com/example/expoplayerapp/MainActivity.java
@@ -102,14 +102,9 @@ public class MainActivity extends AppCompatActivity {
      
         //sorry i can not share url.
 //     
-        //for rotate issue on android 8.1 video 1811
-      //  String videoURL = "any camera recorded video video which has 90 degree rotation";
         
-        //for MediaCodecVideoRenderer: Video codec error
-    //  java.lang.IllegalStateException
-      //  at android.media.MediaCodec.native_dequeueOutputBuffer(Native Method)
         
-         String videoURL = "https://thegoatappvideos.s3.us-east-2.amazonaws.com/post_video/productio.mp4 ";
+         String videoURL = " ";
 
 
         DefaultLoadControl loadControl = new DefaultLoadControl
